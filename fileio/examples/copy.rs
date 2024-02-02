85aea871f8d37115ba5b7c5956ba351425abc81d
Yuichi Morisaki <m0ris4q1@gmail.com>
2024-02-02 20:36:23 +0900

    Add file IO ex. - copy

--- fileio/examples/copy.rs
+++ fileio/examples/copy.rs
@@ -1,15 +1,48 @@
 use alt::fs::OpenOptions;
-use std::io::Read;
+use anyhow::{Context, Result};
+use std::env;
+use std::io::{Read, Write};
+use tlpi::{fatal, usage_err};
 
-fn main() {
-    let mut file = OpenOptions::new()
+fn main() -> Result<()> {
+    let argv: Vec<String> = env::args().collect();
+    if argv.len() != 3 || argv[1] == "--help" {
+        usage_err!("{} old-file new-file\n", argv[0]);
+    }
+
+    /* Open input and output files */
+
+    let mut input_file = OpenOptions::new()
         .read(true)
-        .open("Cargo.toml").unwrap();
+        .open(&argv[1])
+        .with_context(|| format!("opening file {}", argv[1]))?;
+
+    let mut output_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .mode(0o600)
+        .open(&argv[2])
+        .with_context(|| format!("opening file {}", argv[2]))?;
+
+
+    /* Transfer data until we encounter end of input or an error */
 
     let mut buffer = [0; 1024];
-    let n = file.read(&mut buffer[..]).unwrap();
 
-    let content = String::from_utf8_lossy(&buffer[..n]);
+    loop {
+        let num_read = input_file.read(&mut buffer).context("read")?;
+        if num_read == 0 {
+            break;
+        }
+        let num_written = output_file.write(&buffer[..num_read]).context("write")?;
+        if num_written != num_read {
+            fatal!("could not write whole buffer");
+        }
+    }
 
-    println!("{}", content);
+    input_file.close().context("close input")?;
+    output_file.close().context("close output")?;
+
+    Ok(())
 }
